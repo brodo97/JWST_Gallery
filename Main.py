6c72ec05c1e72163d5625783ecb56cb65b892bff
Mattia Brunelli <mattia.brunelli@eurotecsrl.com>
2022-11-23 11:51:17 +0100

    Various fixed, extended resource collection

--- Main.py
+++ Main.py
@@ -10,17 +10,17 @@ known_resources = []
 CONN = sqlite3.connect("Database.db", check_same_thread=False)
 
 
-def get_known_news():
+def get_known_resources():
     """
-    Populate {known_news}
+    Populate {known_resources}
     :return: None
     """
 
-    global known_news
+    global known_resources
 
     # Query known IDs
-    for row in CONN.execute("SELECT ID FROM News;"):
-        known_news.append(row[0])
+    for row in CONN.execute("SELECT ID FROM Resources;"):
+        known_resources.append(row[0])
 
     return
 
@@ -56,7 +56,7 @@ def send_news():
 
     # Query unsent messages
     for row in CONN.execute(
-        "SELECT ID, Title, Description, ImageURL, Link FROM News WHERE Sent = 0;"
+        "SELECT ID, Title, Description, ImageURL, Link FROM Resources WHERE Sent = 0;"
     ):
         news_id, title, description, image_url, link = row
 
@@ -86,12 +86,12 @@ def send_news():
 
         # If response "ok" status is False, send error to admin
         if response["ok"] is False:
-            send_error_to_admin(f"News({news_id})" + response["description"])
+            send_error_to_admin(f"Resources({news_id})" + response["description"])
             break
 
         # Else, update the sell value of the news to the corresponding message ID (on Telegram)
         CONN.execute(
-            "UPDATE News SET Sent = ? WHERE ID = ?;",
+            "UPDATE Resources SET Sent = ? WHERE ID = ?;",
             [response["result"]["message_id"], news_id]
         )
 
@@ -101,26 +101,7 @@ def send_news():
     return
 
 
-def get_data():
-    """
-    Get JWST's news from {URL}
-
-    :return: Execution status, data
-    :rtype: tuple[bool, str]
-    """
-
-    # Make a get request to the URL
-    response = requests.get(url=NEWS_URL)
-
-    # If the response status code differ from 200, then something occurred. Return an error
-    if response.status_code != 200:
-        return False, f"get_data() returned status code = {response.status_code}"
-
-    # Return data
-    return True, response.text
-
-
-def parse_data(data: str):
+def parse_resources(data: str):
     """
     Parse JWST news data into something readable (for the script)
 
@@ -129,7 +110,7 @@ def parse_data(data: str):
     :rtype: tuple[bool, str]
     """
 
-    global known_news
+    global known_resources
 
     # Convert text result into BeautifulSoup4 object
     page = Soup(data, "html.parser")
@@ -159,13 +140,16 @@ def parse_data(data: str):
             # Generate a news id
             news_id = link.split("/")[-1]
 
-            if news_id in known_news:
-                known_news.append(news_id)
+            # If the news is known, continue
+            if news_id in known_resources:
                 continue
+
+            # Else, remember it!
+            known_resources.append(news_id)
 
             # Update news DB
             CONN.execute(
-                "INSERT INTO News (ID, Title, Description, ImageURL, Link) VALUES (?, ?, ?, ?, ?);",
+                "INSERT INTO Resources (ID, Title, Description, ImageURL, Link) VALUES (?, ?, ?, ?, ?);",
                 [news_id, title, description, img, link]
             )
 
@@ -179,31 +163,121 @@ def parse_data(data: str):
     return True, ""
 
 
+def parse_articles(data: str):
+    """
+    Parse JWST articles data into something readable (for the script)
+
+    :param data: JWST article data
+    :return: Execution status, Execution info
+    :rtype: tuple[bool, str]
+    """
+
+    global known_resources
+
+    # Convert text result into BeautifulSoup4 object
+    page = Soup(data, "html.parser")
+
+    try:
+        # Cycle all divs, with class = "news-listing"
+        # reversed is used to sort div from less to more recent
+        for div in reversed(page.find_all("div", {"class": "news-listing"})):
+            # Get article's title
+            title = div.find("h3").text
+
+            # Get article's description
+            description = div.find("div", {"class": "article-description"}).text.strip()
+
+            # Get article's image srcset
+            img = div.find("img").get("srcset")
+
+            # Extract 1 src from the set
+            img = img.split(", ")[0]
+            img = img.split(" ")[0]
+            img = img.replace("//", "https://")
+
+            # Get article's link
+            link = div.find("a").get("href")
+            link = link.split("?")[0]
+
+            # Generate an article ID
+            article_id = link.split("/")[-1]
+
+            # If the article is known, continue
+            if article_id in known_resources:
+                continue
+
+            # Else, remember it!
+            known_resources.append(article_id)
+
+            # Update news DB
+            CONN.execute(
+                "INSERT INTO Resources (ID, Title, Description, ImageURL, Link) VALUES (?, ?, ?, ?, ?);",
+                [article_id, title, description, img, link]
+            )
+
+        # Commit changes
+        CONN.commit()
+
+    except Exception as e:  # If an exception occur, rollback and return the error
+        CONN.rollback()
+        return False, f"parse_data() something went wrong: {e}"
+
+    return True, ""
+
+
+def get_resources():
+    """
+    Get JWST's latest news from {RESOURCES_URL}/*
+
+    :return: Execution status, Execution info
+    :rtype: tuple[bool, str]
+    """
+
+    # Open a session to make the requests all together
+    with requests.Session() as session:
+        # For every different resource type
+        for path in ["images", "videos", "articles", "other-resources"]:
+            # Make a get request to the URL
+            response = session.get(url=RESOURCES_URL + path)
+
+            # If the response status code differ from 200, then something occurred. Return an error
+            if response.status_code != 200:
+                return False, f"get_data() returned status code = {response.status_code} on path: {path!r}"
+
+            # Parse JWST data
+            # If path = "articles", parse the data using a different function, the webpage structure differ, so a
+            # different method of data extraction must be used
+            if path == "articles":
+                result, data = parse_articles(data=response.text)
+            else:
+                result, data = parse_resources(data=response.text)
+
+            # An error occurred while parsing data?
+            if result is False:
+                send_error_to_admin(data)
+
+                # Close DB
+                CONN.close()
+
+                exit()
+
+    # Return ok
+    return True, ""
+
+
 def main():
     """
     Main process
     :return: None
     """
 
-    # Init {known_news} array
-    get_known_news()
+    # Init {known_resources} array
+    get_known_resources()
 
-    # Get JWST data
-    result, data = get_data()
+    # Get JWST news
+    result, data = get_resources()
 
     # An error occurred while getting data?
-    if result is False:
-        send_error_to_admin(data)
-
-        # Close DB
-        CONN.close()
-
-        exit()
-
-    # Parse JWST data
-    result, data = parse_data(data)
-
-    # An error occurred while parsing data?
     if result is False:
         send_error_to_admin(data)
 
@@ -242,5 +316,3 @@ def lambda_handler(event, lambda_context):
 
 if __name__ == "__main__":
     main()
-
-
