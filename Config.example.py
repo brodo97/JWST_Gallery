112f5f5bc9de15685414bf45e9667a885364f60f
Mattia Brunelli <mattia.brunelli@eurotecsrl.com>
2022-12-01 15:15:51 +0100

    New CI/CD Tests

--- Config.example.py
+++ Config.example.py
@@ -1,19 +1,21 @@
 # TELEGRAM CONFIGURATION
 # Admin ID, for errors alert
-TELEGRAM_ADMIN_ID = 0
+import os
+
+TELEGRAM_ADMIN_ID = os.environ["TELEGRAM_ADMIN_ID"]
 # Channel Name, for error alert (formatting)
-TELEGRAM_CHANNEL_NAME = ""
+TELEGRAM_CHANNEL_NAME = os.environ["TELEGRAM_CHANNEL_NAME"]
 # Channel ID, to send news
-TELEGRAM_CHANNEL_ID = -0
+TELEGRAM_CHANNEL_ID = os.environ["TELEGRAM_CHANNEL_ID"]
 # Bot token, the bot that send news to the channel
-TELEGRAM_BOT_TOKEN = ""
+TELEGRAM_BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 
 # MONGODB CONFIGURATION
 # MongoDB Connection URI
-MONGODB_URI = ""
+MONGODB_URI = os.environ["MONGODB_URI"]
 # MongoDB Authentication Certificate
-MONGODB_CERTIFICATE = ""
+MONGODB_CERTIFICATE = os.environ["MONGODB_CERTIFICATE"]
 # Database Name
-MONGODB_DATABASE = ""
+MONGODB_DATABASE = os.environ["MONGODB_DATABASE"]
 # Database's Collection Name
-MONGODB_COLLECTION = ""
+MONGODB_COLLECTION = os.environ["MONGODB_COLLECTION"]
